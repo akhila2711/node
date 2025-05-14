2f96b3c0f6883bb5d72ab08f6ea09042cdd36010
akhila2711 <akhilakhi112799@gmail.com>
2025-05-15 05:27:31 +0530

    add

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const http= require('http');
 
 const server = http.createServer((req, res) =>{
     res.setHeader('content-type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>Node Js</title></head>');
+    res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+    res.write('</html>');
     console.log(req.method)
 });
 server.listen(8000)
