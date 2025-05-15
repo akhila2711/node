93ee87b0e5627e1140cc26afb3fd25bd416c67b5
akhila2711 <akhilakhi112799@gmail.com>
2025-05-15 07:07:29 +0530

    add

--- app.js
+++ app.js
@@ -1,16 +1,31 @@
 const http= require('http');
-
+const fs= require('fs');
 
 const server = http.createServer((req, res) =>{
 
     const url=req.url
+    const method=req.method
+       //ROUTING RESPONSE
     if(url==='/'){
         res.setHeader('content-type', 'text/html');
         res.write('<html>');
         res.write('<head><title>Form data</title></head>');
-        res.write("<body><form action='/message' method='post'><input type='text' name='message'><input type='submit' value='send'></input></form></body>");
+        res.write("<body><form action='/message' method='POST'><input type='text' name='message'><input type='submit' value='send'></input></form></body>");
         res.write('</html>');
         return res.end();
+    }
+    //Redirecting Response
+    if(url==='/message' && method === 'POST'){
+        fs.writeFile("message.txt", "DUMMY", (err) => {
+            if (err) {
+                console.error(err);
+                return;
+            }
+            console.log("file created");
+            res.setHeader('Location', '/');
+            res.statusCode = 302;
+            return res.end();
+        });
     }
     res.setHeader('content-type', 'text/html');
     res.write('<html>');
@@ -18,7 +33,7 @@ const server = http.createServer((req, res) =>{
     res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
     res.write('</html>');
     res.end();
-   //ROUTING RESPONSE
+
 });
 
 server.listen(8000)
