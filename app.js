1c7a7b8282a0d385dd45e76ead30c36043b4db84
akhila2711 <akhilakhi112799@gmail.com>
2025-05-15 05:51:04 +0530

    add

--- app.js
+++ app.js
@@ -2,11 +2,23 @@ const http= require('http');
 
 
 const server = http.createServer((req, res) =>{
+
+    const url=req.url
+    if(url==='/'){
+        res.setHeader('content-type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title>Form data</title></head>');
+        res.write("<body><form action='/message' method='post'><input type='text' name='message'><input type='submit' value='send'></input></form></body>");
+        res.write('</html>');
+        return res.end();
+    }
     res.setHeader('content-type', 'text/html');
     res.write('<html>');
     res.write('<head><title>Node Js</title></head>');
     res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
     res.write('</html>');
-    console.log(req.method)
+    res.end();
+   //ROUTING RESPONSE
 });
+
 server.listen(8000)
