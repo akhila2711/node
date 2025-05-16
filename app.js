d0074abca07ea8cccfc3c285f5d4723f394caf04
akhila2711 <akhilakhi112799@gmail.com>
2025-05-16 09:16:28 +0530

    add

--- app.js
+++ app.js
@@ -10,29 +10,52 @@ const server = http.createServer((req, res) =>{
         res.setHeader('content-type', 'text/html');
         res.write('<html>');
         res.write('<head><title>Form data</title></head>');
-        res.write("<body><form action='/message' method='POST'><input type='text' name='message'><input type='submit' value='send'></input></form></body>");
+        res.write("<body><form action='/message' method='POST'><input type='text' name='message'><input type='file' name='file'><input type='submit' value='send'></input></form></body>");
         res.write('</html>');
         return res.end();
     }
     //Redirecting Response
     if(url==='/message' && method === 'POST'){
-        fs.writeFile("message.txt", "DUMMY", (err) => {
-            if (err) {
-                console.error(err);
-                return;
-            }
-            console.log("file created");
-            res.setHeader('Location', '/');
-            res.statusCode = 302;
-            return res.end();
+        const body=[];
+        req.on('data', (chunk) => {
+            // console.log("chunk")
+            body.push(chunk);
+            console.log(chunk);
         });
-    }
-    res.setHeader('content-type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>Node Js</title></head>');
-    res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
-    res.write('</html>');
-    res.end();
+    req.on('end', () => {
+        const parsedBody = Buffer.concat(body).toString();  // Optionally, parse the body if needed
+        console.log(parsedBody);
+        const message = parsedBody.split('='); // Extract the message from the parsed body
+
+        fs.writeFile("HELLO.txt", message[] || '', (err) => {
+            if (err) {
+                console.error("Error writing file:", err);
+            }
+
+
+            // Optionally, you can write a different file here
+            // For example, writing a different message to "message.txt"
+            fs.writeFile("message.txt", "Welcome to home", (err) => {
+                if (err) {
+                    console.error("Error writing file:", err);
+                } else {
+                    console.log("file created");
+                }
+                res.statusCode = 302;
+                res.setHeader('Location', '/');
+                return res.end();
+            });
+        });
+    });
+    // Prevent further response handling
+    return;
+}
+res.setHeader('content-type', 'text/html');
+res.write('<html>');
+res.write('<head><title>Node Js</title></head>');
+res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+res.write('</html>');
+res.end();
 
 });
 
---
+++ index.js
@@ -0,0 +1,34 @@
+const url = require('url');
+// const address = "https://localhost:3000/home?user=akhi";
+// const parsedUrl = new URL(address);
+
+// console.log(parsedUrl.hostname); // 'localhost'
+// console.log(parsedUrl.pathname); // '/home'
+// console.log(parsedUrl.searchParams.get('user')); // 'akhi'
+
+// import os from 'os';
+// console.log("OS Type", os.type());
+// console.log("OS Platform", os.platform());
+// console.log("Total Memory", os.totalmem());
+// console.log("Free Memory", os.freemem());
+// console.log("CPU Architecture", os.arch());
+// console.log("CPU Info", os.cpus());
+// console.log("Network Interfaces", os.networkInterfaces());
+// console.log("Home Directory", os.homedir());
+// console.log("Uptime", os.uptime());
+// console.log("OS Release", os.release());
+// console.log("OS Version", os.version());
+// console.log("OS Load Average", os.loadavg());
+// console.log("OS Constants", os.constants);
+
+// console.log("OS Endianness", os.endianness());
+// console.log("OS Temp Directory", os.tmpdir());
+
+// console.log("OS User Info", os.userInfo());
+// console.log("OS Release", os.release());
+// console.log("OS Type", os.type());
+// console.log("OS Platform", os.platform());
+// console.log("OS Architecture", os.arch());  
+// console.log("OS Network Interfaces", os.networkInterfaces());
+
+
